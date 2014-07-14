80ec9965aea8b1d83623657b031f477890ec6588
Vijith Venkatesh <vij2288@gmail.com>
2014-07-13 22:27:28 -0400

    Update SecondViewController.m

--- SecondViewController.m
+++ SecondViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by Vijith Venkatesh on 4/12/14.
 //  Copyright (c) 2014 Vijith Venkatesh. All rights reserved.
-//  Group members Vijith, Adwaith, Shreya, Aditya
+
 
 // this file is responsible to display the settings screen
 
