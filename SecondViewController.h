c3375e69127c14f261b5524f9f6b9ab39ebad25b
Vijith Venkatesh <vij2288@gmail.com>
2014-07-13 22:27:54 -0400

    Update SecondViewController.h

--- SecondViewController.h
+++ SecondViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by Vijith Venkatesh on 4/12/14.
 //  Copyright (c) 2014 Vijith Venkatesh. All rights reserved.
-//  Group members Vijith, Adwaith, Shreya, Aditya
+
 
 
 #import <UIKit/UIKit.h>
