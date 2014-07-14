a2f89aacac50abd42b76af55707736fe12c9a3c8
Vijith Venkatesh <vij2288@gmail.com>
2014-07-13 22:28:21 -0400

    Update QnA.m

--- QnA.m
+++ QnA.m
@@ -4,7 +4,7 @@
 //
 //  Created by Vijith Venkatesh on 4/13/14.
 //  Copyright (c) 2014 Vijith Venkatesh. All rights reserved.
-//  Group members Vijith, Adwaith, Shreya, Aditya
+
 
 
 #import "QnA.h"
